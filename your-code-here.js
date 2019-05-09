30b28248f7cf9b6b06481593202cc3c172a8610a
Danny Navejas <dannynavejas@gmail.com>
2019-05-08 21:21:34 -0500

    created functions to move car up, down, and left

--- your-code-here.js
+++ your-code-here.js
@@ -1,10 +1,29 @@
+let increment = 8;
 function tick() {
     let car = document.querySelector('#car'); //get reference to car
-    moveLeft(car, 10); //move the car left
+    moveUp(car, increment); //move the car left
 }
 
-function moveLeft(element, increment) {
+function moveRight(element, increment) {
     let props = getProps(element); //get the positional information you need to move an element
     console.dir(props); //check the console for what props has in it!
     element.style.left = (props.left + increment) + 'px'; //move element
+}
+function moveDown(element,increment){
+    let props = getProps(element); //get the positional information you need to move an element
+    console.dir(props); //check the console for what props has in it!
+    element.style.top = (props.top + increment) + 'px'; //move element
+}
+function moveLeft(element,increment){
+    let props = getProps(element); //get the positional information you need to move an element
+    console.dir(props); //check the console for what props has in it!
+    element.style.left = (props.left - increment) + 'px'; //move element
+}
+function moveUp(element,increment){
+    let props = getProps(element); //get the positional information you need to move an element
+    console.dir(props); //check the console for what props has in it!
+    element.style.top = (props.top - increment) + 'px'; //move element
+}
+function makeTurn(element){
+    let props = getProps(element);
 }

6ad2942fa7bfc4f7907695b85cbb03e2746041e1
Danny Navejas <dannynavejas@gmail.com>
2019-05-09 21:26:52 -0500

    car stops after crossing finish line and interval stops running

--- your-code-here.js
+++ your-code-here.js
@@ -1,57 +1,49 @@
-let increment = 4;
+let increment = 6;
 function tick() {
     let car = document.querySelector('#car'); //get reference to car
     drive(car, increment); //move the car left
 }
-
 function moveRight(element, increment) {
     let props = getProps(element); //get the positional information you need to move an element
-    // console.dir(props); //check the console for what props has in it!
     element.style.left = (props.left + increment) + 'px'; //move element
 }
 function moveDown(element,increment){
     let props = getProps(element); //get the positional information you need to move an element
-    // console.dir(props); //check the console for what props has in it!
     element.style.top = (props.top + increment) + 'px'; //move element
 }
 function moveLeft(element,increment){
     let props = getProps(element); //get the positional information you need to move an element
-    // console.dir(props); //check the console for what props has in it!
     element.style.left = (props.left - increment) + 'px'; //move element
 }
 function moveUp(element,increment){
     let props = getProps(element); //get the positional information you need to move an element
-    // console.dir(props); //check the console for what props has in it!
     element.style.top = (props.top - increment) + 'px'; //move element
 }
 function getPastFinishLine(element,increment){
     let props = getProps(element); //get the positional information you need to move an element
-    // console.dir(props); //check the console for what props has in it!
     element.style.left = (props.left + increment) + 'px'; //move element
 }
 function drive(element, increment){
     let props = getProps(element);
     console.log(props);
-    console.log(element);
-    console.log(increment);
 
-    if(props.left >= 600 && props.top >= 100 && props.top <= 135){
+    if(props.left >= 250 && props.top <= 120 && props.left <= 265){
+        stop();
+    }
+    else if(props.left >= 600 && props.top >= 121 && props.top <= 135 && props.left <= 810){
         moveRight(element, increment);
     }
-    if (props.left >= 810 && props.top >= 100 && props.top <= 310){    
+    else if (props.left >= 810 && props.top >= 110 && props.top <= 310){    
         element.setAttribute("class", "r90");
         moveDown(element, increment);
     } 
-    else if(props.left <= 820 && props.top >= 260 && props.top <= 315 && props.left >= 115){
+    else if(props.left <= 820 && props.top >= 260 && props.top <= 320 && props.left >= 115){
         element.setAttribute("class", "r180");
         moveLeft(element, increment);
     }
-    else if(props.left <= 200 && props.top >= 120 && props.top <= 315){
+    else if(props.left <= 200 && props.top >= 120 && props.top <= 320){
         element.setAttribute("class", "r270");
         moveUp(element, increment);
-    }
-    else if(props.left >= 250 && props.top <= 120 && props.left <= 265){
-        clearInterval;
     }
     else if(props.left >= 100 && props.top <= 120 && props.left <= 260){
         element.setAttribute("class", "");
